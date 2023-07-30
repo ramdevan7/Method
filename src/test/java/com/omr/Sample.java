f8412dcb216501a4e3bdf2ae02e89e08ebaa6343
ramdevan7 <ramdevan7@gmail.com>
2023-07-30 11:47:15 +0530

    Ubdate Codes

---
+++ src/test/java/com/omr/Sample.java
@@ -0,0 +1,14 @@
+package com.omr;
+
+public class Sample {
+	private void methode1() {
+    System.out.println("methode1");
+	}
+	private void methode2() {
+	    System.out.println("methode2");
+		}
+	private void methode3() {
+	    System.out.println("methode3");
+		}
+
+}
